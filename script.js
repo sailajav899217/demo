f59e888c6dd0f03f5221e99fb693d6a03f3f2a23
sailajav899217 <vadali_sailaja@yahoo.com>
2025-12-30 13:26:19 +0530

    script.js  added

---
+++ script.js
@@ -0,0 +1,10 @@
+// filepath: c:\Users\vssai\OneDrive\Desktop\gitclasses\script.js
+
+// Example: Simple function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+// Add your code below
